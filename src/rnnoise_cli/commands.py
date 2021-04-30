db614f01b3ad186429a5eae2fc815cee7852bfd8
Inias Peeters <iniasp@gmail.com>
2021-04-30 13:12:16 +0200

    Color change 🎨

--- src/rnnoise_cli/commands.py
+++ src/rnnoise_cli/commands.py
@@ -9,6 +9,7 @@ import configparser
 ANSI_COLOR_GREEN = "\u001b[32m"
 ANSI_COLOR_RED = "\u001b[31m"
 ANSI_COLOR_BLUE = "\u001b[34m"
+ANSI_COLOR_YELLOW = "\u001b[33m"
 ANSI_UNDERLINE = "\u001b[4m"
 ANSI_NO_UNDERLINE = "\u001b[24m"
 ANSI_STYLE_RESET = "\u001b[0m"
@@ -22,6 +23,7 @@ CONFIG_DEFAULTS = {
         "control": "50"
     }
 }
+
 
 class AliasedGroup(click.Group):
     """
@@ -60,7 +62,7 @@ def echo_devices_pretty():
     devices = PulseInterface.get_input_devices()
     for d in devices:
         click.echo(
-            f"[{ANSI_COLOR_GREEN}{d.index}{ANSI_STYLE_RESET}] "
+            f"[{ANSI_COLOR_YELLOW}{d.index}{ANSI_STYLE_RESET}] "
             f"{ANSI_COLOR_BLUE}{d.name}{ANSI_STYLE_RESET} "
             f"({d.description})"
         )
@@ -69,7 +71,7 @@ def echo_devices_pretty():
 def prompt_device_pretty() -> str:
     echo_devices_pretty()
     return click.prompt(
-        f"{ANSI_COLOR_GREEN}Number{ANSI_STYLE_RESET} or "
+        f"{ANSI_COLOR_YELLOW}Number{ANSI_STYLE_RESET} or "
         f"{ANSI_COLOR_BLUE}name{ANSI_STYLE_RESET} "
         f"of device to use",
         default=PulseInterface.get_default_input_device().index,
